936a1bb1b821b0c819ea8f5b349b85dc170156ea
Lindsay <51120285+loslds@users.noreply.github.com>
2024-04-01 18:21:27 -0300

    segunda

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import Login from './components/pages/Login';
 import Login1 from './components/pages/Login1';
 import Login2 from './components/pages/Login2';
 import Login3 from './components/pages/Login3';
-//import Login4 from './components/pages/Login4';
+import Login4 from './components/pages/Login4';
 
 import { Recepcao } from './components/pages/Recepcao';
 import { Design } from './components/pages/Design';
@@ -33,7 +33,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLAreaElement).render(
           <Route path="/login1" element={<Login1 />} />
           <Route path="/login2" element={<Login2 />} />
           <Route path="/login3" element={<Login3 />} />
-
+          <Route path="/login4" element={<Login4 />} />
           {/*<Route path="/login4" element={<Login4 />} /> */}
 
           <Route path="/tabprc" element={<TabPrc />} />
